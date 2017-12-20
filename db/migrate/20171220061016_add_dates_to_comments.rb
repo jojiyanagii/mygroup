85764c378341197de0af58e5ca50665401854848
Joji Yanagii <yanagiijoji@gmail.com>
2017-12-20 06:16:40 +0000

    Add date

---
+++ db/migrate/20171220061016_add_dates_to_comments.rb
@@ -0,0 +1,5 @@
+class AddDatesToComments < ActiveRecord::Migration[5.1]
+  def change
+    add_column :comments, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213140630) do
+ActiveRecord::Schema.define(version: 20171220061016) do
 
   create_table "comments", force: :cascade do |t|
     t.string "tag"
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
